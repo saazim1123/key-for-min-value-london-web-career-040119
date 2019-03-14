90903abe478dff7bdf343f4f2cd8c610755b6dbf
Adnan Azim <saazim1123@gmail.com>
2019-03-14 04:23:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   min_key = nil 
   min_value = nil
   name_hash.each do |key, value|
-    if min_value == nil || value <
+    if min_value == nil || value < min_value
       
     end
   end
